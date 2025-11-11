f5ecded078b96fb1e6792f28ae67a1f512d50071
HeinsAlarde <20220026725@my.xu.edu.ph>
2025-11-11 21:27:57 +0800

    Added add, substract, multiply, and divide functions

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# Simple Calculator in Python
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero!"
+    return x / y
